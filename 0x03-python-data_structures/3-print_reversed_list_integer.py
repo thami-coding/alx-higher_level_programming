68ee20d09863e93d8b3739f3232a2cd77a2ec769
Thami <sainttsquared@example.com>
2022-09-27 03:12:23 +0200

    added new file

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    stop = len(my_list) + 1
-    for i in range(1, stop):
-        print("{:d}".format(my_list[-i]))
+    for i in range(0, len(my_list)):
+        print("{:d}".format(my_list.pop()))
---
+++ 0x03-python-data_structures/7-main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+if len(a) < 1:
+if len(b) < 1
