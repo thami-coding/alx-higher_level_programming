48d83242dce89f8d1dcbcba4f5ef1accd7775837
Thami <sainttsquared@example.com>
2022-09-28 18:22:43 +0200

    added new file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for item in my_list:
-        return [x if x is not search else replace for x in my_list ]
+        return [x if x is not search else replace for x in my_list]
