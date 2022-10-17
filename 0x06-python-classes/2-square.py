372113187adeb43b625944d598f8ba2f4af55cd2
Thami <sainttsquared@example.com>
2022-10-17 11:38:07 +0200

    added new file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,18 @@
+#!/usr/bin/python3
+""" Square module"""
+
+
 class Square:
+    """Square class"""
     def __init__(self, size=0):
         self.set_size(size)
 
-    def set_size(self, size):
-        if type(size) is not int:
+    def set_size(self, s):
+        if not isinstance(s, int):
+            raise TypeError("size must be an integer")
+        if s < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = s
 
+    def get_size(self):
+        return self.__size
