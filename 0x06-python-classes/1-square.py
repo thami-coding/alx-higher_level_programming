991d28d7651094d83dc80524ada0895152a91705
Thami <sainttsquared@example.com>
2022-10-17 11:13:30 +0200

    added new file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
 class Square:
+    """square class that initializes a square"""
     def __init__(self, size):
         self.__size = size
 
     def set_size(self, size):
         self.__size = size
+
     def get_size(self):
         return self.__size
