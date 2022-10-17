6d7c791dc397ddab204c3e049c000e33ffbddad4
Thami <sainttsquared@example.com>
2022-10-17 09:45:44 +0200

    added new file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as exc:
+    except BaseException as exc:
         print("Exception: {}".format(exc), file=sys.stderr)
         return False
-
-
