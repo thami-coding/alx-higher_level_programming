b43e0a344d9fa43e91c24950aaffd6070e464540
Thami <sainttsquared@example.com>
2022-10-17 11:45:00 +0200

    added new file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Square module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        self.set_size(size)
+
+    def set_size(self, s):
+        if not isinstance(s, int):
+            raise TypeError("size must be an integer")
+        if s < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = s
+
+    def area(self):
+        return self.__size * self.__size
