8d9fbdda816b3e15156f4272ee06b1bef8f6e974
Thami <sainttsquared@example.com>
2022-10-18 05:50:28 +0200

    modified file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,7 +42,7 @@ class Square:
         if self.__size == 0:
             print()
         val1, val2 = self.__position
-        if val1 == 1 and val2 == 1:
+        if val1 == 1 or val2 == 1:
             print()
         for i in range(self.__size):
             for k in range(self.__position[0]):
