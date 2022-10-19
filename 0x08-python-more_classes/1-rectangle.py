4dfb1a66c24e2411063a2c0c7ae873e93533c97d
Thami <sainttsquared@example.com>
2022-10-19 05:25:17 +0200

    added new file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """Rectangle Class"""
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def height(self):
