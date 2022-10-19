eb4c6fea4d90f35c62551dfd0b4c158233a25bb2
Thami <sainttsquared@example.com>
2022-10-19 19:51:11 +0200

    modified file

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,8 +43,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__width == 0 or self.__height == 0:
-            print()
-            return
+            return ""
         for i in range(self.__height):
             for j in range(self.__width):
                 string += "#"
