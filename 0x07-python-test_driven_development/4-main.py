c886981450ed53b7eda45cb70bec6e1a12012eab
Thami <sainttsquared@example.com>
2022-10-24 11:56:56 +0200

    added new file

---
+++ 0x07-python-test_driven_development/4-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print_square = __import__('4-print_square').print_square
+
+
+print_square(0)
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+print_square module
+This module has on function print_square(size) that prinsts a square
+"""
+
+
+def print_square(size):
+    """
+    size must be an integer, otherwise
+    raises a TypeError exception
+    if size is a float and is less than 0,
+    raise a TypeError exception
+    prints square using #
+    """
+    if size == 0:
+        print()
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    for row in range(size):
+        for col in range(size):
+            print("#", end="")
+        print()
