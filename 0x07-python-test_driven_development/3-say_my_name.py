e2ac27d26458c2a71905b87da2e527588f367807
Thami <sainttsquared@example.com>
2022-10-24 08:16:34 +0200

    added new file

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is the prints  module.
+it has one function, prints(first_name, last_name)
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    first_name and last_name must
+    be strings otherwise, raises a
+    TypeError exception
+
+    prints My name is <first name> <last name>
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
