1d37ed7e77fda541bc1bc9b3bfd23b4fd411b4b8
Thami <sainttsquared@example.com>
2022-10-25 11:43:37 +0200

    corrections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 my_list module.
-MyList inherits from list and has one method of its own
+MyList inherits from list and has one method
+called print_sorted
 """
 
 
@@ -9,7 +10,8 @@ class MyList(list):
 
     def print_sorted(self):
         """
-        print list in ascending order
+        print list in ascending order wihtout
+        changing the original list
         """
         new_list = list(self)
         new_list.sort()
