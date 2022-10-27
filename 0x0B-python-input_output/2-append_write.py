5bee32f2f3924f307fb5cd7b18a1dd2f2a6a6ebc
Thami <sainttsquared@example.com>
2022-10-27 07:43:24 +0200

    added new file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -12,4 +12,4 @@ def append_write(filename="", text=""):
     number of characters added
     """
     with open(filename, 'a', encoding='utf-8') as f:
-       return  f.write(text)
+        return f.write(text)
