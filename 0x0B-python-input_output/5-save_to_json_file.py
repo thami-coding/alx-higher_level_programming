d66e0c124085e5e8db09c5e3e03c55393cd1d717
Thami <sainttsquared@example.com>
2022-10-27 08:19:50 +0200

    added new file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module has one function
+save_to_json_file(my_obj, filename)
+that writes an Object to a text file,
+using a JSON representation
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an Object to a text file,
+    using a JSON representation
+    """
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
