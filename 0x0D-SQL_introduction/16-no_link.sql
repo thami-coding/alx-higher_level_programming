c472a9ab8111af1fa97e027fc50eb7e6d5d964f3
Thami <sainttsquared@gmail.com>
2022-12-01 17:43:25 +0200

    corrections

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,5 @@
 -- lists all records of the table second_table
 -- of the database hbtn_0c_0
 -- lists all records of the table
-SELECT score,
-    name
-FROM second_table
-WHERE name IS NOT NULL ORDER BY DESC;
+SELECT score,name FROM second_table
+WHERE name IS NOT NULL ORDER BY score DESC;
