ded8edb4601878d0d5c70de1ecfb68e3e65d9a65
Thami <sainttsquared@gmail.com>
2022-12-07 19:04:51 +0200

    added new file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
+ -- creates the database hbtn_0d_2 and the user user_0d_2
 -- creates a new database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a new user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- gives user SELECT privilege
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';io   + 
