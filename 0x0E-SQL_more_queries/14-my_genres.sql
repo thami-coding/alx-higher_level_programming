10e8b4a9dc9c908c1e0e508931e024126339f544
Thami <sainttsquared@gmail.com>
2022-12-08 07:06:35 +0200

    added new file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,7 @@
 SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
     JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    JOIN tv_shows
-    ON tv_show_genres.show_id = 8
+    WHERE tv_show_genres.show_id = 8
     ORDER BY name ASC;
+
+-- 
