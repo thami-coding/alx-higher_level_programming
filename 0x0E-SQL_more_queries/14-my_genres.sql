9940267f83d8ce14ba849fa96c0cff3e21e8190e
Thami <sainttsquared@gmail.com>
2022-12-08 17:34:26 +0200

    corrections

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,11 @@
 SELECT DISTINCT tv_genres.name AS name
 FROM tv_genres
     JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    WHERE tv_show_genres.show_id = 8
+    WHERE tv_show_genres.show_id = 
     ORDER BY name ASC;
+
+
+
+    
 
 -- 
