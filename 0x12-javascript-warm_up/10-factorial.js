69a36b4ec6f3301a42fc7dee53e939d19f7b61cc
Thami <sainttsquared@gmail.com>
2022-12-20 06:07:17 +0200

    add corrections

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const num = process.argv[2]
-function factorial(n) {
+const num = process.argv[2];
+function factorial (n) {
   if (n === 0) {
-    return 1
+    return 1;
   }
-  return n * factorial(n - 1)
+  return n * factorial(n - 1);
 }
 if (parseInt(num)) {
-  console.log(factorial(num))
+  console.log(factorial(num));
 } else {
-  console.log(1)
+  console.log(1);
 }
