daeb240d15a69261190c9ec9470f269ed8293b34
Thami <sainttsquared@gmail.com>
2022-12-20 05:54:40 +0200

    add corrections

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ let str = '';
 
 if (parseInt(x)) {
   for (let i = 0; i < x; i++) {
-    for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++) {
       str += 'X';
     }
     console.log(str);
