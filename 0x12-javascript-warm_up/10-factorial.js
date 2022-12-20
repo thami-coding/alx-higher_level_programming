67ed6da9699767b765f45a2f7b3e5a3755859add
Thami <sainttsquared@gmail.com>
2022-12-20 06:05:22 +0200

    add  script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const num = process.argv[2]
+function factorial(n) {
+  if (n === 0) {
+    return 1
+  }
+  return n * factorial(n - 1)
+}
+if (parseInt(num)) {
+  console.log(factorial(num))
+} else {
+  console.log(1)
+}
