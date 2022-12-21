d9c97f61d285972faa619a1cf7e7cc7ad79b696f
Thami <sainttsquared@gmail.com>
2022-12-21 23:29:13 +0200

    make pycodestyle compliant

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=username, passwd=password, db=database,
                          host="localhost", port=3306)
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC""")
+    c.execute("""SELECT * FROM states
+                WHERE name LIKE 'N%'
+                ORDER BY states.id ASC""")
     results = c.fetchall()
     for row in results:
         print(row)
