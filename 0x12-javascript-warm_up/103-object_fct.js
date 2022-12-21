88b050f0dee10648808a4f47c6b1c6739c6bf5e5
Thami <sainttsquared@gmail.com>
2022-12-21 12:38:49 +0200

    corrections

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
+  value: 12
 };
 console.log(myObject);
 
-myObject.incr = function() {
-	this.value++;
-
-}
+myObject.incr = function () {
+  this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
