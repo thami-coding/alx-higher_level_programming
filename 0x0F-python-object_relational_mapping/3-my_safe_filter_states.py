66aa051a7c30016359e87d147e3773e7ac41f22d
Thami <sainttsquared@gmail.com>
2022-12-23 00:19:31 +0200

    add corrections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,16 +8,21 @@ import sys
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    pattern = sys.argv[4]
-    db = MySQLdb.connect(user=username, passwd=password, db=database,
-                         host="localhost", port=3306)
-    c = db.cursor()
-    c.execute("""SELECT * FROM states
-                 WHERE name = '{}'
-                 ORDER BY states.id ASC""".format(pattern))
-    results = c.fetchall()
-    for row in results:
-        print(row)
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        pattern = sys.argv[4]
+        db = MySQLdb.connect(user=username, passwd=password, db=database,
+                             host="localhost", port=3306)
+        c = db.cursor()
+        c.execute(
+                     """
+                     SELECT * FROM states
+                     WHERE name = '{}'
+                     ORDER BY states.id ASC
+                     """.format(pattern)
+                   )
+        results = c.fetchall()
+        for row in results:
+            print(row)
