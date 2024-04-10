faa7d72226f10ac603d1cf0e00663810abd76639
thami-coding <thamig125@gmail.com>
2024-04-10 18:57:04 +0200

    made rectangle executable

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,8 +19,9 @@ class Rectangle:
     def width(self, width):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width <= 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = width
 
     @property
@@ -31,6 +32,6 @@ class Rectangle:
     def height(self, height):
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if height <= 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
