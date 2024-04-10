7a2f339920f5a96b900eec485885e9ffcd39467c
thami-coding <thamig125@gmail.com>
2024-04-10 18:45:48 +0200

    done

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""module contains one Rectangle class based on 0-rectangle.py"""
+
+
+class Rectangle:
+    """Rectangel class that contains width and height of rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
