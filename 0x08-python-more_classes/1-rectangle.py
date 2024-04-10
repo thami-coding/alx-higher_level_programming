307bb13a6f1d598da19437064390e46ea92e323b
thami-coding <thamig125@gmail.com>
2024-04-10 18:59:41 +0200

    added setter in init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     """Rectangel class that contains width and height of rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
