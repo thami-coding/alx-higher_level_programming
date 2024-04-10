0389208bd54cf4f109c0af5498a8300acbe28b2f
thami-coding <thamig125@gmail.com>
2024-04-10 20:39:45 +0200

    formated file

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle:
     """Rectangel class calculates area and perimeter of rectangle"""
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
