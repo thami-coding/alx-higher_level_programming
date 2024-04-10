c435af18c5715bd8bad685f2e2472ef6b0924856
thami-coding <thamig125@gmail.com>
2024-04-10 22:10:04 +0200

    modified file

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -73,9 +73,9 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        elif type(rect_2) != Rectangle:
+        if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1.area() >= rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             rect_2
