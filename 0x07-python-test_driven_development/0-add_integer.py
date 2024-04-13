1d3d1a35a7bb79bb225907eddf1e89ff789ae13a
thami-coding <thamig125@gmail.com>
2024-04-13 03:03:33 +0200

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Modules has one function add_integer"""
+
+
 def add_integer(a, b=98):
     """a function that adds 2 integers."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
-         raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
 
     if isinstance(a, float):
         a = int(a)
