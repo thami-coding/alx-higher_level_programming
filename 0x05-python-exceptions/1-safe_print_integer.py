3f0dd5ef97f418b313beb84498f8161a15fb1c5e
thami-coding <thamig125@gmail.com>
2024-04-14 18:00:30 +0200

    done task

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,18 @@
+def safe_print_integer(value):
+    """
+    Print the first 'x' elements of a list safely.
+
+    Parameters:
+    - my_list (list, optional): The input list containing
+      elements of any type. Defaults to an empty list.
+    - x (int, optional): The number of elements to print
+    from the list. Defaults to 0.
+
+    Returns:
+    - int: The real number of elements printed.
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
