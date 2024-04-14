a920fcf6bac5c169153ea9973dd33b707a06f7fd
thami-coding <thamig125@gmail.com>
2024-04-14 18:20:39 +0200

    corections

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,24 @@
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Print the first 'x' integers from a list.
+
+    Parameters:
+    - my_list (list, optional): The input list containing
+    elements of any type. Defaults to an empty list.
+    - x (int, optional): The number of elements to access
+    from the list. Defaults to 0.
+
+    Returns:
+    - int: The real number of integers printed.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end=' ')
+                count += 1
+    except IndexError as err:
+        print(err)
+    finally:
+        print()
+    return count
