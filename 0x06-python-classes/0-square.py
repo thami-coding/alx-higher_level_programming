cd95aaafc2ed595bd31bd308bc93643399107ed2
thami-coding <thamig125@gmail.com>
2024-04-15 00:40:29 +0200

    corections

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module provides a representation of a square.
+
+Classes:
+- Square: Represents an empty square.
+"""
+
+
+class Square:
+    """
+    Represents an empty square.
+
+    This class doesn't have any attributes or methods
+    defined. It serves as a placeholder for
+    potential future functionality related to squares.
+    """
+    pass
