d6185a6f39203be8c62f49a832d67bce658dbfaf
thami-coding <thamig125@gmail.com>
2024-04-14 17:44:08 +0200

    corection

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,18 +11,6 @@ first 'x' elements of a list safely.
 
 
 def safe_print_list(my_list=[], x=0):
-    """
-    Print the first 'x' elements of a list safely.
-
-    Parameters:
-    - my_list (list, optional): The input list containing
-      elements of any type. Defaults to an empty list.
-    - x (int, optional): The number of elements to print
-    from the list. Defaults to 0.
-
-    Returns:
-    - int: The real number of elements printed.
-    """
     count = 0
     try:
         for i in range(x):
