94f7e2600fa099ebd77e4e81cedba24179dd7113
thami-coding <thamig125@gmail.com>
2024-04-15 19:16:40 +0200

    corections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,18 +3,18 @@
 
 """
 This module defines a class MyList that inherits
-from the built-in list class.
+the built-in list class.
 """
 
 
 class MyList(list):
     """
     Represents a custom list class inheriting
-    from the built-in list class.
+    the built-in list class.
 
     Attributes:
         Inherits all attributes and methods
-        from the built-in list class.
+        the built-in list class.
 
     Methods:
         print_sorted(self): Prints the list
