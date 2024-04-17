33a1749efe0612d101dae04166e329cb45ead77d
thami-coding <thamig125@gmail.com>
2024-04-17 21:09:40 +0200

    corrections

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-r = Rectangle(3, 5)
+print(issubclass(Rectangle, BaseGeometry))
 
-print(r)
-print(dir(r))
+# #!/usr/bin/python3
+# Rectangle = __import__('8-rectangle').Rectangle
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# r = Rectangle(3, 5)
 
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+# print(r)
+# print(dir(r))
+
+# try:
+#     print("Rectangle: {} - {}".format(r.width, r.height))
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
+
+# try:
+#     r2 = Rectangle(4, True)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,42 +2,7 @@
 
 """Module has two class BaseGeometry and Rectangle"""
 
-
-class BaseGeometry:
-    """
-    Represents a base geometry class.
-
-    Methods:
-        area(self): Raises an Exception with the
-        message 'area() is not implemented'.
-        integer_validator(self, name, value): Validates
-        the given value as an integer.
-    """
-
-    def area(self):
-        """
-        Raises an Exception indicating that the
-        method is not implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates the given value as an integer.
-
-        Parameters:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-
-        if not type(value) is int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
