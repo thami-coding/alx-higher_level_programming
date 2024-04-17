957e09d2280ffccc799ff55d28e36e7c60a80ba9
thami-coding <thamig125@gmail.com>
2024-04-17 07:52:52 +0200

    corrections

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read a text file
+and print its contents to stdout.
+
+Functions:
+    read_file(filename=""): Reads a text file and prints
+    its contents to stdout.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints its contents to stdout.
+
+    Args:
+        filename (str, optional): The path to the file to
+        be read. Defaults to "".
+
+    Raises:
+        FileNotFoundError: If the specified file is not found.
+        Exception: If an error occurs during file reading.
+
+    Returns:
+        None
+    """
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            for line in file:
+                print(line, end="")
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print("An error occurred:", e)
