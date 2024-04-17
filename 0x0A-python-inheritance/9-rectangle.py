efd8daa721409456a9467184eee984f5efb2ed47
thami-coding <thamig125@gmail.com>
2024-04-17 21:11:52 +0200

    corrections

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -34,7 +34,7 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0.
         """
 
-        if not type(value) == int:
+        if not type(value) is int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
