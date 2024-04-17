91f320b8931e8cf335efb52f580b5be56d0b993c
thami-coding <thamig125@gmail.com>
2024-04-17 21:03:08 +0200

    corrections

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -21,4 +21,4 @@ def inherits_from(obj, a_class):
         subclass of a_class (directly or indirectly),
         False otherwise.
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
