182562690946838e877cdf75b9f017bea44a1f39
Thami <sainttsquared@gmail.com>
2024-05-24 12:37:19 +0200

    done

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- script that creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa) on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE,name VARCHAR(256));
